fb1d98ad20c0cc02b0673def912f3d712dcd95d1
leand <leand@LEANDRONOTE>
2016-11-12 21:17:01 -0200

    Add project files.

---
+++ WindowsService/FileCreationSvc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService
+{
+    public partial class FileCreationSvc : ServiceBase
+    {
+        public FileCreationSvc()
+        {
+            InitializeComponent();
+            InitializeSheduler();
+        }
+        private void InitializeSheduler()
+        {
+            Sheduler oSheduler = new Sheduler();
+            oSheduler.Start();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ WindowsService/Sheduler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService
+{
+    class Sheduler
+    {
+        Timer oTimer = null;
+        double interval = 20000;
+
+        public void Start()
+        {
+            oTimer = new Timer(interval);
+            oTimer.AutoReset = true;
+            oTimer.Enabled = true;
+            oTimer.Start();
+
+            oTimer.Elapsed += new ElapsedEventHandler(oTimer_Elapsed);
+
+        }
+
+        private void oTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            FileCreation();
+        }
+
+        void FileCreation()
+        {
+            string sFilePath = @"C:\Users\leand\Desktop\file\ShedulerFile.txt";
+            DateTime sDateTime = DateTime.Now;
+            StreamWriter oFileWriter = new StreamWriter(sFilePath,true);
+            oFileWriter.WriteLine($"\n {sDateTime}");
+            oFileWriter.Close();
+
+        }
+    }
+}
